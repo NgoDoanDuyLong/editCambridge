a09b9bc27d2d2d253af8668d8367f6b690c87a7a
NgoDoanDuyLong <116696389+NgoDoanDuyLong@users.noreply.github.com>
2022-10-26 10:27:35 +0700

    Update cambridge.js

--- cambridge.js
+++ cambridge.js
@@ -36,7 +36,7 @@ class DuyLongCambridge {
                 return node.innerText.trim();
         }
 
-        let base = 'https://dictionary.cambridge.org/vi/dictionary/english-vietnamese/';
+        let base = 'https://dictionary.cambridge.org/vi/dictionary/english/';
         let url = base + encodeURIComponent(word);
         let doc = '';
         try {
